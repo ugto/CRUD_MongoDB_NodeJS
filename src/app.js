d6041fda78f732371fb4872814c28131550c2027
Tarsicio Ramos <t.ramos@ugto.mx>
2022-09-28 22:59:26 -0500

    Se agrego la configuracionde handlebars y se segmento la parte de navegacion del sitio

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import express from "express";
-import indexRoutes  from './routes/index';
+import exphbs from "express-handlebars";
+import indexRoutes  from "./routes/index";
+import path from 'path';
+import { create } from 'express-handlebars';
+
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+
+app.engine(
+    ".hbs",
+    create({
+      layoutsDir: path.join(app.get("views"), "layouts"),
+      partialsDir: path.join(app.get("views"), "partials"),
+      defaulLayout: "main",
+      extname: ".hbs",
+    }).engine
+  );
+
+app.set("view engine",".hbs");
 
 app.use(indexRoutes);
-
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,18 @@ import {Router} from 'express';
 const router = Router();
 
 router.get("/",(req,res)=>{
-    res.send("Pagina de inicio");
+    res.render('index');
+    //res.send("Pagina de inicio");
 });
 
 router.get("/about",(req,res)=>{
-    res.send("Pagina de About");
+    res.render('about');
+    //res.send("Pagina de About");
+});
+
+router.get("/edit",(req,res)=>{
+    res.render('edit');
+    //res.send("Pagina de About");
 });
 
 
